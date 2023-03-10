eee33eadfb913cadb1c61c7c7c326107a67d8a2c
Stetson Ramey <stetson.ramey@gmail.com>
2023-03-10 13:17:05 -0800

    optimize for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [require("postcss-import-ext-glob"), require("postcss-import"), require("tailwindcss")],
+  plugins: [
+    require("postcss-import-ext-glob"),
+    require("postcss-import"),
+    require("tailwindcss"),
+    process.env.NODE_ENV === "production" ? require("cssnano") : "",
+  ],
 };
